a9ebd6e421cbb0aaca737e28ae85ee854a67471f
HavidzUbaidillah <havidzbosky@gmail.com>
2023-06-22 14:43:11 +0700

    keduax

--- resources/views/page5-admin/login-admin.blade.php
+++ resources/views/page5-admin/login-admin.blade.php
@@ -274,6 +274,26 @@
                         </div>
                     </div>
                 @endif
+                @error('username')
+                    <div class="toast align-items-center text-bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true" id="login-error">
+                        <div class="d-flex">
+                            <div class="toast-body">
+                                Minimal masukin username lah bodoh!
+                            </div>
+                            <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                        </div>
+                    </div>
+                    @enderror
+                    @error('password')
+                    <div class="toast align-items-center text-bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true" id="login-error">
+                        <div class="d-flex">
+                            <div class="toast-body">
+                                Password kau bodoh!
+                            </div>
+                            <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                        </div>
+                    </div>
+                    @enderror
                 <div class="section pb-5 pt-5 pt-sm-2 text-center">
                     <div class="card-3d-wrap mx-auto">
                         <div class="card-3d-wrapper">
@@ -284,7 +304,7 @@
                                         <div class="section text-center">
                                             <h4 class="mb-4 pb-3">Login Admin</h4>
                                             <div class="form-group">
-                                                <input type="text" name="username" class="form-control" placeholder="Masukkan username" id="nama" autocomplete="off">
+                                                <input type="text" name="username" class="form-control @error('username') is-invalid @enderror" placeholder="Masukkan username" id="nama" autocomplete="off">
                                                 <i class="input-icon uil uil-at"></i>
                                             </div>
                                             <div class="form-group mt-2">
