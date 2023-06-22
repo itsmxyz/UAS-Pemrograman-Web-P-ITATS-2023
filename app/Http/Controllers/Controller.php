6c58b3f3b7a383b616b7c8365b87ab50f41c5460
latifs24 <latif.s12401@gmail.com>
2023-06-22 14:56:35 +0700

    Pertamax

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,16 +9,16 @@ use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController
 {
-   public final function index(){
+   public function index(){
        if (Auth('schale')->check() || Auth('sensei')->check() || Auth('sekretaris')->check()) {
            if (Auth('schale')->check())
-               $username = Auth('schale')->user()->username;
+               $data = ['username' => Auth('schale')->user()->username];
            elseif (Auth('sensei')->check())
-               $username = Auth('sensei')->user()->username;
+               $data = ['username' => Auth('sensei')->user()->username];
            elseif (Auth('sekretaris')->check())
-               $username = Auth('sekretaris')->user()->username;
+               $data = ['username' => Auth('sekretaris')->user()->username];
 
-           return view('page1-home.home', $username);
+           return view('page1-home.home', $data);
        }
        else
            return view('page1-home.home');
