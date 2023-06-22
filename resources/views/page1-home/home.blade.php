aea70dab229b9fa1ae9c6a41a435da301657a896
HavidzUbaidillah <havidzbosky@gmail.com>
2023-06-22 11:41:26 +0700

    keduax

--- resources/views/page1-home/home.blade.php
+++ resources/views/page1-home/home.blade.php
@@ -48,8 +48,8 @@
                 <li><a class="nav-link scrollto" href="#klub">Klub</a></li>
                 <li><a class="nav-link scrollto" href="#galeri">Galeri</a></li>
                 <li><a class="nav-link scrollto" href="#kontak">Hubungi Kami</a></li>
-                @if(auth()->guard('schale')||auth()->guard('sensei')||auth()->guard('sekretaris') )
-                    <li><a class="nav-link scrollto" href="{{route('logout',['username'=>auth()->guard('sensei')->user()->username])}}">Logout</a></li>
+                @if(auth()->guard('schale')->check()||auth()->guard('sensei')->check()||auth()->guard('sekretaris')->check() )
+                    <li><a class="nav-link scrollto" href="{{route('home.logout')}}">Logout</a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" id="login">Login<i class="bi bi-chevron-down"></i></a>
