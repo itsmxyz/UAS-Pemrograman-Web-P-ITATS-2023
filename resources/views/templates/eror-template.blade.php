cb8edb05257cc774dd18419b5be02c054742c293
HavidzUbaidillah <havidzbosky@gmail.com>
2023-06-26 16:24:26 +0700

    keduax

---
+++ resources/views/templates/eror-template.blade.php
@@ -0,0 +1,10 @@
+
+
+<script>
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+        footer: '<a href="">Why do I have this issue?</a>'
+    })
+</script>
