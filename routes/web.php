78e5919ef3c39796ef47ce1aff3001b553bcd0ec
latifs24 <latif.s12401@gmail.com>
2023-06-30 01:03:29 +0700

    Pertamax

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,7 @@ Route::middleware('schale')->group(function () {
     Route::get('/schale/sekretaris', [AdminController::class, 'getDataSekretaris'])->name('schale.sekretaris');
     Route::get('/schale/siswa', [AdminController::class, 'getDataSiswa'])->name('schale.siswa');
     Route::get('/schale/kelas/all', [AdminController::class,'getDataKelas'])->name('schale.kelas');
+    Route::get('/schale/kelas/{id_kelas}', [AdminController::class,'getDataKelas'])->name('schale.kelas-view');
     Route::post('/schale/sensei/create-sensei', [SenseiController::class, 'store'])->name('schale.sensei-create');
     Route::post('/schale/sensei/update-sensei', [SenseiController::class, 'update'])->name('schale.sensei-update');
     Route::post('/schale/sensei/delete-sensei', [SenseiController::class, 'destroy'])->name('schale.sensei-delete');
