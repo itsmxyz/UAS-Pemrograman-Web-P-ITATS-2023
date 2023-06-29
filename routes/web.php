7b407a54f5db25258ccaa4b29ef660b4f3858bd1
latifs24 <latif.s12401@gmail.com>
2023-06-29 22:09:40 +0700

    Pertamax

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,7 @@ Route::middleware('schale')->group(function () {
     Route::get('/schale/dashboard', [AdminController::class, 'index'])->name('schale.dashboard');
     Route::get('/schale/sensei', [AdminController::class, 'getDataSensei'])->name('schale.sensei');
     Route::get('/schale/sekretaris', [AdminController::class, 'getDataSekretaris'])->name('schale.sekretaris');
+    Route::get('/schale/siswa', [SiswaController::class, 'show'])->name('schale.siswa');
     Route::post('/schale/sensei/create-sensei', [SenseiController::class, 'store'])->name('schale.sensei-create');
     Route::post('/schale/sensei/update-sensei', [SenseiController::class, 'update'])->name('schale.sensei-update');
     Route::post('/schale/sensei/delete-sensei', [SenseiController::class, 'destroy'])->name('schale.sensei-delete');
@@ -55,7 +56,6 @@ Route::middleware('schale')->group(function () {
     Route::post('/schale/sekretaris/update-sekretaris', [SekretarisController::class, 'update'])->name('schale.sekretaris-update');
     Route::post('/schale/sekretaris/delete-sekretaris', [SekretarisController::class, 'destroy'])->name('schale.sekretaris-delete');
     Route::post('/schale/sekretaris/reset-sekretaris', [SekretarisController::class, 'resetPassword'])->name('schale.sekretaris-reset');
-    Route::get('/data-siswa', [SiswaController::class, 'show']);
 });
 
 
